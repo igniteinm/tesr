02bdf7b60b163d23e4eaa4f4be2395babd8ee986
igniteinm <111630059+igniteinm@users.noreply.github.com>
2022-09-02 17:16:23 +0530

    Create create.sh

---
+++ create.sh
@@ -0,0 +1,31 @@
+#build 
+source build/envsetup.sh
+lunch lineage_beryllium-user
+export TZ=Asia/Delhi
+export BUILD_USERNAME=beryllium
+export BUILD_HOSTNAME=user
+export SKIP_ABI_CHECKS=true
+export SKIP_BOOTJAR_CHECKS=true
+
+#
+make bacon -j30 &
+sleep 90m
+kill %1
+
+#until [ $? == 0 ] ;
+#do
+    #sleep 10
+   # make bacon -j30
+#done
+
+#trying to fix oom container error
+#until [ -f /$WORKDIR/rom/$name_rom/out/target/product/beryllium/*.zip ] ;
+#do
+     # make bacon -j30
+    #  sleep 10
+     # echo ROM building complete
+#done
+      #echo move to other task
+
+
+# upload rom
