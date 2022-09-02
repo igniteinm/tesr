3af94404ea5540e569121b3b2c04e9d7317bdbcf
igniteinm <111630059+igniteinm@users.noreply.github.com>
2022-09-02 22:42:53 +0530

    Create loadache.sh

---
+++ loadache.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $WORKDIR
+mkdir -p ~/.config/rclone
+echo "$rcloneconfig" > ~/.config/rclone/rclone.conf
+rclone copy --drive-chunk-size 256M --stats 1s rom:rom/ccache/$name_rom/ccache.tar.gz $WORKDIR -P
+if [ $? -ne 0 ] ;
+then
+   kill %1
+fi
+tar xzf ccache.tar.gz
+rm -rf ccache.tar.gz
