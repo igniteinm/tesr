a5cc553189c6afde4d0ebe15b1337ed1b1a2689b
igniteinm <111630059+igniteinm@users.noreply.github.com>
2022-09-21 20:43:00 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,22 +1,23 @@
 #build 
 source build/envsetup.sh
-lunch lineage_beryllium-user
+lunch arrow_beryllium-user
 export TZ=Asia/Delhi
 export BUILD_USERNAME=beryllium
 export BUILD_HOSTNAME=user
 export SKIP_ABI_CHECKS=true
 export SKIP_BOOTJAR_CHECKS=true
+export ARROW_GAPPS=true
 
 #
-make bacon -j30 #&
-#sleep 95m
-#kill %1
+make bacon -j30 &
+sleep 95m
+kill %1
 
-until [ $? == 0 ] ;
-do
-    sleep 10
-    make bacon -j30
-done
+#until [ $? == 0 ] ;
+#do
+    #sleep 10
+   # make bacon -j30
+#done
 
 #trying to fix oom container error
 #until [ -f /$WORKDIR/rom/$name_rom/out/target/product/beryllium/*.zip ] ;
